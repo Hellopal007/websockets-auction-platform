cb8eaf84b5efe37f4f6fc022e6a6df1c01e35489
hello pal <you@example.com>
2025-08-06 12:06:54 +0530

    Set initial backend

---
+++ Backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+ 
+
+
+app.get("/",(req, res) => {
+  res.status(200).send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+    console.log("Server UP & RUNNING on Port ${PORT} ")
+});
